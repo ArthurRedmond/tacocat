dd676cc29864fcaf2dd675f5d6115a8f0ae8219e
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-10-07 06:40:45 -0400

    Update app page layout

--- js/site.js
+++ js/site.js
@@ -1,4 +1,19 @@
-//Call HelloWorld 
-function HelloWorld() {
-    alert("Hello World");
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let result = checkValue(userString);
+    displayResult(result);
+}
+
+function checkValue(userString) {
+    let result = "";
+    //removes anything not a-z or A-Z
+    let cleanString = userString.replace(/[^a-zA-Z ]/g, "");
+    result = cleanString;
+    return result;
+}
+
+function displayResult(result) {
+    document.getElementById("message").innerHTML = `${result}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
